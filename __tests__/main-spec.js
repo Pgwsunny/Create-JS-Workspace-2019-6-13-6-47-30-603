f09ff0cea2b11f40d7e938233d98eacea9f7fc75
Pgwsunny <814424014@qq.com>
2019-08-20 19:23:59 +0800

    js

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,24 @@
-const add = require('../main');
+const {isValid,generatemultiplyArray} = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+it('should divide two numbers',() =>{
+	//given
+	var firstNumber =2;
+	var secondNumber=3;
+	//when
+	var result=isValid(firstNumber,secondNumber);	
+	//then
+	expect(result).toBe(true);
 });
+
+it('should generated array with table items',() =>{
+	//given
+	var firstNumber =2;
+	var secondNumber=3;
+	//when
+	var generagedArray=generatemultiplyArray(firstNumber,secondNumber);	
+	//then
+	expect(generagedArray[0][0]).toBe('2*2=4');
+	expect(generagedArray[1][0]).toBe('2*3=6');
+	expect(generagedArray[1][1]).toBe('3*3=9');
+});
+
--- main.js
+++ main.js
@@ -1,5 +1,41 @@
-function add (left, right) {
-    return left + right;
+module.exports = {isValid,generatemultiplyArray};
+
+function multiplyTable(firstNumber,secondNumber){
+    var output ="";
+    var valid = isValid(firstNumber,secondNumber)
+    if(valid){
+        return valid;
+    }
+    var multiplyArray = generatemultiplyArray(firstNumber,secondNumber);
+    output = printMultiplyTable(generatemultiplyArray)
 }
 
-module.exports = add;
+function isValid(firstNumber,secondNumber){
+    return firstNumber < secondNumber;
+}
+
+function generatemultiplyArray(firstNumber,secondNumber){
+    // var generagedArray =[];
+    // var times = secondNumber-firstNumber+1;
+    // for (let rowIndex = 0; rowIndex < times; rowIndex++){
+    //     var row =[];
+    //     for(let colunmIndex =0;colunmIndex<(rowIndex+1);colunmIndex++){
+    //         var result = ((colunmIndex+1)*(rowIndex+1))
+    //         row.push(result)
+    //     }
+    // }
+    var result = new Array();
+    for(let i = firstNumber; i < secondNumber + 1; i++){
+        let list = new Array();
+        for(let j = firstNumber; j <= i; j++){
+            let tmpString = j + "*" + i + "=" + i * j;
+            list.push(tmpString);
+        }
+        result.push(list);
+    }
+    return result;
+}
+
+function printMultiplyTable(generatemultiplyArray){
+        console.log(result);
+}
